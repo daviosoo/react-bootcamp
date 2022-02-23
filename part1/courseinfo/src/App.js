8eb39ba21e0751ba576cea4ed77c2b1c9dd94a61
David Osorio <davosorio404@gmail.com>
2022-02-22 22:17:45 -0500

    Refactor code

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,24 +4,37 @@ const Part = (props) => <p> {props.partName} {props.exercises} </p>
 const Content = (props) => {
   return(
     <>
-      <Part partName = {props.parts[0]} exercises={props.exercises[0]}/>
-      <Part partName = {props.parts[1]} exercises={props.exercises[1]}/>
-      <Part partName = {props.parts[2]} exercises={props.exercises[2]}/>
+      <Part partName = {props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part partName = {props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part partName = {props.parts[2].name} exercises={props.parts[2].exercises}/>
     </>
   )
 }
 
-const Total = ({exercises})=> <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
+const Total = (props)=> <p>Number of exercises {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises}</p>
+
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
-  const exercises = [10, 7, 14]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Title course={course}/>
-      <Content parts={parts} exercises={exercises}/>
-      <Total exercises={exercises}/>
+      <Content parts={parts}/>
+      <Total exercises={parts}/>
     </div>
   )
 }
